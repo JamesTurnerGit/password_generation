569c289077fb4a6f2874e740405f247429b4bdc3
James Turner <mail.jt@gmail.com>
2017-09-29 11:35:57 +0100

    first test

---
+++ lib/generatePassword.rb
@@ -0,0 +1,3 @@
+def generatePassword
+  "something"
+end
---
+++ spec/generatePassword_spec.rb
@@ -0,0 +1,6 @@
+require 'generatePassword'
+describe '#generatePassword' do
+  it 'returns something' do
+    expect(generatePassword).not_to be_nil
+  end
+end
