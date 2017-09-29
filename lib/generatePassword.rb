c1b7fe8548b53cd9909f4d7fd7da640b7297e250
James Turner <mail.jt@gmail.com>
2017-09-29 11:44:03 +0100

    made length 'work'

--- lib/generatePassword.rb
+++ lib/generatePassword.rb
@@ -1,3 +1,8 @@
-def generatePassword length, uppercase, lowercase, number, special
-  "something"
+def generatePassword length,
+                     uppercase,
+                     lowercase = false,
+                     number = false,
+                     special = false
+  "x" * length
+  #UPPERCASE = "abcdefghijklmnopqrstuvwxyz"
 end
--- spec/generatePassword_spec.rb
+++ spec/generatePassword_spec.rb
@@ -3,4 +3,9 @@ describe '#generatePassword' do
   it 'accepts five arguments' do
     expect{generatePassword(5,true,false,false,false)}.not_to raise_error
   end
+  it 'uses first var and returns something of the same length' do
+    length = rand(0..10)
+    password = generatePassword(length,true,false,false,false)
+    expect(password.length).to eq length
+  end
 end
