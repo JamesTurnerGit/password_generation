c5c3d78cd40a343db828aba8c6898641dad1689f
James Turner <mail.jt@gmail.com>
2017-09-29 11:56:52 +0100

    started actual password gen

--- lib/generatePassword.rb
+++ lib/generatePassword.rb
@@ -1,8 +1,19 @@
+UPPERCASE_LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+#LOWERCASE_LETTERS = "abcdefghijklmnopqrstuvwxyz"
+#NUMBER = "0123456789"
+#SPECIAL = "!$%&*@^"
+
 def generatePassword length,
                      uppercase,
                      lowercase = false,
                      number = false,
                      special = false
-  "x" * length
-  #UPPERCASE = "abcdefghijklmnopqrstuvwxyz"
+                     
+  valid_characters = UPPERCASE_LETTERS
+
+  def getCharacter valid_characters
+    valid_characters[rand(0..valid_characters.length)]
+  end
+
+  getCharacter(valid_characters) * length
 end
